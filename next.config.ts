1de7300509f4f78598c52c5a8bba9804fa1b1143
Nykyta Koblianskyi <nkoblianskyi@gmail.com>
2025-04-11 21:37:23 +0200

    1

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://rbl.palladium.expert/:path*',
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
