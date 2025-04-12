0948ae67158884c0e87a9a6129715ef957361245
Nykyta Koblianskyi <nkoblianskyi@gmail.com>
2025-04-12 14:25:32 +0200

    1

--- next.config.ts
+++ next.config.ts
@@ -2,32 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
 
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'https://rbl.palladium.expert/:path*',
-      },
-    ];
-  },
-
-  async headers() {
-    return [
-      {
-        source: '/api/:path*',
-        headers: [
-          {
-            key: 'Content-Type',
-            value: 'application/json',
-          },
-          {
-            key: 'Auth-Database',
-            value: 'your_database_name',
-          },
-        ],
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
