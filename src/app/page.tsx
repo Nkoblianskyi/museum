9a5b4b52d1b738a8475bf23ecc65c2439b0450ee
Nykyta Koblianskyi <nkoblianskyi@gmail.com>
2025-04-14 21:22:52 +0200

    1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,13 +6,11 @@ import Facts from '../components/Facts';
 import Diversity from '../components/Diversity';
 import Events from '../components/Events';
 import CTA from '../components/CTA';
-import CloakClient from '@/components/CloakClient';
 
 export default function Home() {
   
   return (
     <div className="flex flex-col w-full flex-1 overflow-x-hidden">
-      <CloakClient />
       <Hero />
       <WhyItMatters />
       <Heritage />
